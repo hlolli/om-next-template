59747fd02328500127e69e74e40e3a3dc6ea2397
hloedver <hlolli-work@dhcp-187.berlin.lambdawerk.com>
2016-09-19 16:22:54 +0200

    bump

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
-                 [org.clojure/clojurescript "1.9.89"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
+                 [org.clojure/clojurescript "1.9.229"]
                  [com.cognitect/transit-clj "0.8.285"]
                  [com.cognitect/transit-cljs "0.8.239"]
                  [ring "1.5.0"]
                  [compojure "1.5.1"]
                  [figwheel-sidecar "0.5.4-7"]
-                 [org.omcljs/om "1.0.0-alpha41"]
+                 [org.omcljs/om "1.0.0-alpha45"]
                  [com.stuartsierra/component "0.3.1"]
                  [com.cemerick/piggieback "0.2.1"]]
 
