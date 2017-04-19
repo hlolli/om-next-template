2b9df12016a47b7d4d0ba147db0001a93655144c
hlolli <hlolli@gmail.com>
2017-04-19 17:09:36 +0200

    bump

--- project.clj
+++ project.clj
@@ -4,20 +4,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [com.cognitect/transit-clj "0.8.285"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                 [org.clojure/clojurescript "1.9.521"]
+                 [com.cognitect/transit-clj "0.8.300"]
                  [com.cognitect/transit-cljs "0.8.239"]
-                 [ring "1.5.0"]
-                 [compojure "1.5.1"]
-                 [figwheel-sidecar "0.5.9"]
-                 [org.omcljs/om "1.0.0-alpha47"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [com.cemerick/piggieback "0.2.1"]]
+                 [ring "1.6.0-RC3"]
+                 [compojure "1.6.0-beta3"]
+                 [figwheel-sidecar "0.5.10"]
+                 [org.omcljs/om "1.0.0-alpha48"]
+                 [com.stuartsierra/component "0.3.1"]]
 
-  :plugins [[lein-cljsbuild "1.1.4"]
-            [lein-figwheel "0.5.9"]
-            [lein-ring "0.9.7"]]
+  :plugins [[lein-cljsbuild "1.1.5"]
+            [lein-figwheel "0.5.10"]
+            [lein-ring "0.11.0"]]
 
   :ring {:handler backend.server/handler
          :port 8440}
